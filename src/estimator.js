675e7f6cc1e64e9a3c791bf1ba79ef8a536c9e08
Rockstar Developer <34315690+kenmbelenzi@users.noreply.github.com>
2020-04-11 17:13:57 +0300

    Revert "Assesment"

--- src/estimator.js
+++ src/estimator.js
@@ -35,36 +35,17 @@ const covid19ImpactEstimator = (data) => {
   impact.severeCasesByRequestedTime = 0.15 * impact.infectionsByRequestedTime;
   severeImpact.severeCasesByRequestedTime = 0.15 * severeImpact.infectionsByRequestedTime;
 
-  const bedsAvailable = Math.trunc(0.35 * Inputdata.totalHospitalBeds);
+  const bedsAvailable = 0.35 * Inputdata.totalHospitalBeds;
 
   impact.hospitalBedsByRequestedTime = bedsAvailable - impact.severeCasesByRequestedTime;
   // eslint-disable-next-line max-len
   severeImpact.hospitalBedsByRequestedTime = bedsAvailable - severeImpact.severeCasesByRequestedTime;
-
-  // challenge 3
-  impact.casesForICUByRequestedTime = 0.05 * impact.infectionsByRequestedTime;
-  severeImpact.casesForICUByRequestedTime = 0.05 * severeImpact.infectionsByRequestedTime;
-
-  impact.casesForVentilatorsByRequestedTime = 0.02 * impact.infectionsByRequestedTime;
-  severeImpact.casesForICUByRequestedTime = 0.02 * severeImpact.infectionsByRequestedTime;
-  let rate;
-  if (data.periodType === 'days') rate = data.timeToElapse;
-  else if (data.periodType === 'weeks') rate = (data.timeToElapse * 7);
-  else if (data.periodType === 'months') rate = (data.timeToElapse * 30);
-  const avgIncomeUsd = Inputdata.region.avgDailyIncomeInUSD;
-  const avgIncomePop = Inputdata.region.avgDailyIncomePopulation;
-
-  // eslint-disable-next-line max-len
-  impact.dollarsInflight = Math.trunc((impact.infectionsByRequestedTime * avgIncomeUsd * avgIncomePop) / rate);
-  // eslint-disable-next-line max-len
-  severeImpact.dollarsInflight = Math.trunc((severeImpact.infectionsByRequestedTime * avgIncomeUsd * avgIncomePop) / rate);
 
   return {
     data: input,
     impact: {},
     severeImpact: {}
   };
-  // eslint-disable-next-line no-unreachable
 };
 
 export default covid19ImpactEstimator;
