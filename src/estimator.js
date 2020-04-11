ff565c166210e4e619b6575a2f77e801da6db50c
ken <kenmbelenzi@gmail.com>
2020-04-11 14:41:08 +0300

    challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,27 @@
+const InputData = {
+  region: {
+    name: 'Africa',
+    avgAge: 19.7,
+    avgDailyIncomeInUSD: 5,
+    avgDailyIncomePopulation: 0.71
+  },
+  periodType: 'days',
+  timeToElapse: 58,
+  reportedCases: 674,
+  population: 66622705,
+  totalHospitalBeds: 1380614
+};
+const impact = {
+  currentlyInfected: InputData.reportedCases * 10,
+  infectionsByRequestedTime: this.currentlyInfected * Math.pow(2,9)
+}
+
+const servereImpact= {
+  currentlyInfected: InputData.reportedCases * 50,
+  infectionsByRequestedTime: this.currentlyInfected * Math.pow(2,9),
+  severeCasesByRequestedTime: this.infectionsByRequestedTime*0.15
+
+}
 const covid19ImpactEstimator = (data) => data;
 
-export default covid19ImpactEstimator;
+
